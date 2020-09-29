9572b2e099d4e0d032b13093fbe4c81f7faef135
IanFade <ifade@cs.stonybrook.edu>
2020-09-29 19:50:03 -0400

    Add comment describing bitboard layout

--- Classier/include/Bitwise.h
+++ Classier/include/Bitwise.h
@@ -4,6 +4,19 @@
 #include <inttypes.h>
 #define bitBoard uint64_t
 
+// LAYOUT #2
+// http://cinnamonchess.altervista.org/bitboard_calculator/Calc.html
+/*
+56	57	58	59	60	61	62	63
+48	49	50	51	52	53	54	55
+40	41	42	43	44	45	46	47
+32	33	34	35	36	37	38	39
+24	25	26	27	28	29	30	31
+16	17	18	19	20	21	22	23
+08	09	10	11	12	13	14	15
+00	01	02	03	04	05	06	07
+*/
+
 enum PieceType : char
 {
     Empty,
